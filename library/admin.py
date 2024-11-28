bbfc374e12e3e0972d84914135902df690ca0309
ckul <88058944+ckuly@users.noreply.github.com>
2024-11-28 10:59:36 +0200

    Changed various things, in admin.py and models.py, added database entires in db.sqlite3

--- library/admin.py
+++ library/admin.py
@@ -1,8 +1,35 @@
 from django.contrib import admin
-
 from .models import Author, Genre, Book, BookInstance
 
-admin.site.register(Book)
-admin.site.register(Author)
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    readonly_fields = ('id', 'due_back', 'status')
+    can_delete = False
+    extra = 0  # išjungia papildomas tuščias eilutes įvedimui
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'display_books')
+
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back')
+    list_filter = ('status', 'due_back')
+    list_editable = ('due_back', 'status')
+    search_fields = ('id', 'book__title')
+
+    fieldsets = (
+        ('General', {'fields': ('id', 'book')}),
+        ('Availability', {'fields': ('status', 'due_back')}),
+    )
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+admin.site.register(BookInstance, BookInstanceAdmin)
--- library/models.py
+++ library/models.py
@@ -4,16 +4,25 @@ import uuid
 class Genre(models.Model):
     name = models.CharField('Pavadinimas', max_length=200, help_text='Įveskite knygos žanrą (pvz. detektyvas)')
 
+    class Meta:
+        verbose_name = 'Žanras'
+        verbose_name_plural = 'Žanrai'
+
     def __str__(self):
         return self.name
 
 class Book(models.Model):
     """Modelis reprezentuoja knygą (bet ne specifinę knygos kopiją)"""
     title = models.CharField('Pavadinimas', max_length=200)
-    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True, related_name='books')
     summary = models.TextField('Aprašymas', max_length=1000, help_text='Trumpas knygos aprašymas')
     isbn = models.CharField('ISBN', max_length=13, help_text='13 Simbolių <a href="https://www.isbn-international.org/content/what-isbn">ISBN kodas</a>')
     genre = models.ManyToManyField(Genre, help_text='Išrinkite žanrą(us) šiai knygai')
+
+    def display_genre(self):
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Žanras'
 
     def __str__(self):
         return self.title
@@ -49,6 +58,11 @@ class Author(models.Model):
     """Model representing an author."""
     first_name = models.CharField('Vardas', max_length=100)
     last_name = models.CharField('Pavardė', max_length=100)
+
+    def display_books(self):
+        return ', '.join(book.title for book in self.books.all()[:3])
+
+    display_books.short_description = 'Knygos'
 
     class Meta:
         ordering = ['last_name', 'first_name']
