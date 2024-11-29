ff4984b08bef78c9a43e33ecb5794ab49063bbc8
ckul <88058944+ckuly@users.noreply.github.com>
2024-11-29 11:30:02 +0200

    Puncts done did

--- library/views.py
+++ library/views.py
@@ -1,5 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Book, BookInstance, Author, Genre
 
 def index(request):
-    return HttpResponse("Labas, pasauli!")
+    # Suskaičiuokime keletą pagrindinių objektų
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Laisvos knygos (tos, kurios turi statusą 'g')
+    num_instances_available = BookInstance.objects.filter(status__exact='g').count()
+
+    # Kiek yra autorių
+    num_authors = Author.objects.count()
+
+    # perduodame informaciją į šabloną žodyno pavidale:
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    # renderiname index.html, su duomenimis kintamąjame context
+    return render(request, 'index.html', context=context)
