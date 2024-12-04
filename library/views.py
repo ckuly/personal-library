bc1d3c5e10e0f0caab0724a9d93ca5b402efb463
ckul <88058944+ckuly@users.noreply.github.com>
2024-12-04 13:42:46 +0200

    All things created

--- library/views.py
+++ library/views.py
@@ -4,25 +4,24 @@ from .models import Book, BookInstance, Author
 from django.views import generic
 from django.db.models import Q
 
+
 def index(request):
-    # Suskaičiuokime keletą pagrindinių objektų
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-    # Laisvos knygos (tos, kurios turi statusą 'g')
     num_instances_available = BookInstance.objects.filter(status__exact='g').count()
-
-    # Kiek yra autorių
     num_authors = Author.objects.count()
 
-    # perduodame informaciją į šabloną žodyno pavidale:
+    # Papildome kintamuoju num_visits, įkeliame jį į kontekstą.
+
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
-
-    # renderiname index.html, su duomenimis kintamąjame context
     return render(request, 'index.html', context=context)
 
 
